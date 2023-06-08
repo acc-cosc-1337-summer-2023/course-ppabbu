32ab2466b555c89b4474315dac53b75ef49abe3c
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-06-08 22:09:42 +0000

    HW 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,22 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
-	
-
+	int num;
+	cout<<"Enter the Number=";
+	cin>>num;
+	int result;
+	result = multiply_numbers(num);
+	cout<<"Result = "<<result<<endl;
+	int num1 = 4;
+    result = multiply_numbers(num1);
+	cout<<"Result = "<<result<<endl;
 	return 0;
 }
--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -5,9 +5,9 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify sum_numbers function") 
+TEST_CASE("Test Multiply numbers") 
 {
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
+	REQUIRE(multiply_numbers(10) == 50);
+	REQUIRE(multiply_numbers(2) == 10);
 }
 
