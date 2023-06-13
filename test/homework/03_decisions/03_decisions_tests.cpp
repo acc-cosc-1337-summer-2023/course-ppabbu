6b3c42b7f41ee94b845963c217d59f5188e2b2c2
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-06-13 17:29:54 +0000

    wrote test cases

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -1,7 +1,50 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+
 }
 
+TEST_CASE("Verify Grade A if","Verification") {
+	REQUIRE(get_letter_grade_using_if(95) =='A');
+}
+
+TEST_CASE("Verify Grade B if","Verification") {
+	REQUIRE(get_letter_grade_using_if(85) =='B');
+}
+
+TEST_CASE("Verify Grade C if","Verification") {
+	REQUIRE(get_letter_grade_using_if(75) =='C');
+}
+
+TEST_CASE("Verify Grade D if","Verification") {
+	REQUIRE(get_letter_grade_using_if(65) =='D');
+}
+
+TEST_CASE("Verify Grade F if ","Verification") {
+	REQUIRE(get_letter_grade_using_if(55) =='F');
+}
+
+
+
+TEST_CASE("Verify Grade A switch","Verification") {
+	REQUIRE(get_letter_grade_using_switch(95) =='A');
+}
+
+TEST_CASE("Verify Grade B switch","Verification") {
+	REQUIRE(get_letter_grade_using_switch(85) =='B');
+}
+
+TEST_CASE("Verify Grade C switch","Verification") {
+	REQUIRE(get_letter_grade_using_switch(75) =='C');
+}
+
+TEST_CASE("Verify Grade D switch","Verification") {
+	REQUIRE(get_letter_grade_using_switch(65) =='D');
+}
+
+TEST_CASE("Verify Grade F switch","Verification") {
+	REQUIRE(get_letter_grade_using_switch(55) =='F');
+}
