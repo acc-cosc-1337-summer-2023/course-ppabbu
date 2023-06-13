561596f62415ffe5cfa0d5963f6c5f65129f97a1
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-06-13 16:30:24 +0000

    Completed get_letter_grade_using_if

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,26 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+char get_letter_grade_using_if(int grade)
+{
+    char letter;
+
+    if(grade>89) {
+        letter = 'A';
+    }
+    else if(grade>79) {
+        letter = 'B';
+    }
+    else if(grade>69) {
+         letter = 'C';
+    }
+    else if(grade>59) {
+        letter = 'D';
+    }
+    else{
+        letter = 'F';
+    }
+
+    return letter;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,15 @@
 //write include statements
 
+#include "decisions.h"
+
+#include <iostream>
+
 int main() 
 {
+
+	char letter = get_letter_grade_using_if(97);
+
+	std::cout << "grade is:" << letter << std::endl;
+
 	return 0;
 }
