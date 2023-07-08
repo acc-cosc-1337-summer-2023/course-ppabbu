0d1143cad7d4024c57eb467c5d6bc6294be8fcf2
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-07-08 15:23:51 +0000

    Pending changes exported from your codespace

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,62 @@
 //cpp
+#include "tic_tac_toe.h"
+
+using std::cout;
+
+bool TicTacToe::game_over()
+{
+    return check_board_full();
+}
+
+void TicTacToe::start_game(std::string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+void TicTacToe::display_board() const
+{
+    for(long unsigned int i=0; i < pegs.size(); i += 3)
+    {
+        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+    }
+}
+
+void TicTacToe::clear_board()
+{
+    for(auto& peg: pegs)
+    {
+        peg = " ";
+    }
+}
+
+void TicTacToe::set_next_player()
+{
+    if(player == "X")
+    {
+        player = "Y";
+    }
+    else
+    {
+        player = "X";
+    }
+}
+
+bool TicTacToe::check_board_full()
+{
+    for(int i=0; i < pegs.size(); i++)
+    {
+        if(pegs[i]==" ")
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
--- src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
@@ -1,5 +1,5 @@
 #include "tic_tac_toe_4.h"
-
+#
 /*
 class function check_column_win
 Win by column if and return true if (each column index)
