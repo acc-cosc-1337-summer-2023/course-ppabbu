7cacc46dfb8963d98953934921955b21aa28764c
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-07-09 04:03:33 +0000

    did test cases for tic tac toe tests

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test if the board is full")
 {
-	TicTacToe game;
+	TicTacToe game;   
 
 	game.mark_board(1);
 	REQUIRE(game.game_over()==false);
