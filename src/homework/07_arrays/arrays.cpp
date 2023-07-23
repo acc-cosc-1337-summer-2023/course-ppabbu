212c8882cb062b03966b59a8f062c8cb1195beb0
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-07-23 04:50:04 +0000

    HW 8 adjust elements

--- src/homework/07_arrays/arrays.cpp
+++ src/homework/07_arrays/arrays.cpp
@@ -1,1 +1,73 @@
-//add function(s) code here
+//cpp
+#include <iostream>
+#include "arrays.h"
+using namespace std;
+ArrayList::ArrayList(int* array_list, int capacity) :
+    elements(array_list)
+{
+    this->capacity = capacity;
+}
+
+void ArrayList::Add(int value)
+{
+    if(current_index < capacity)
+    {
+        elements[current_index] = value;
+        current_index++;
+    }
+}
+
+int ArrayList::Get_First_Index_Of(int value)
+{
+    auto index = -1;
+    auto local_current_index = 0;
+    
+    while(local_current_index < current_index)
+    {
+        if(elements[local_current_index] == value)
+        {
+            index = local_current_index;
+            break;
+        }
+
+        local_current_index++;
+    }
+
+    return index;
+}
+
+int ArrayList::Get_Value_At_Index(int index)
+{
+    int value = -1;
+
+    if(index >= 0 && index < current_index)
+    {
+        value = elements[index];
+    }
+
+    return value;
+}
+
+void ArrayList::Delete(int value)
+{
+    auto index = Get_First_Index_Of(value);
+
+    if(index != -1)
+    {
+        AdjustElements(index);
+    }
+}
+
+void ArrayList::AdjustElements(int index)
+{
+    if(index < 0 || index >= current_index)
+    {
+        return;
+    }
+    //run loop to move array elements to the left and then reduce array by 1
+    for(int i = index; i < current_index; i++)
+    {
+        elements[i] = elements[i+1];
+    }
+    current_index--;
+}
--- test/homework/07_arrays/07_arrays_tests.cpp
+++ test/homework/07_arrays/07_arrays_tests.cpp
@@ -1,7 +1,137 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "arrays.h"
+#include "arrays.cpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test arraylist capacity")
+{
+	const int SIZE = 3;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Capacity() == 3);
+}
+
+TEST_CASE("Test arraylist size")
+{
+	const int SIZE = 3;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+}
+
+TEST_CASE("Test arraylist Add")
+{
+	const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+
+	array_list.Add(8);
+
+	REQUIRE(array_list.Size() == 1);
+
+	array_list.Add(5);
+	REQUIRE(array_list.Size() == 2);
+
+	array_list.Add(20);
+	REQUIRE(array_list.Size() == 3);
+}
+
+TEST_CASE("Test array list get first index of")
+{
+
+	const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+	array_list.Add(8);
+	array_list.Add(5);
+	array_list.Add(20);
+	array_list.Add(5);
+
+	int index = array_list.Get_First_Index_Of(5);
+
+	REQUIRE(index == 1);
+
+	index = array_list.Get_First_Index_Of(20);
+
+	REQUIRE(index == 2);
+}
+
+TEST_CASE("Test get value at index")
+{
+const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+	array_list.Add(8);
+	array_list.Add(5);
+	array_list.Add(20);
+	array_list.Add(5);
+
+	int value = array_list.Get_Value_At_Index(1);
+	REQUIRE(value == 5);
+}
+
+TEST_CASE("Test get value at index w -1")
+{
+const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+	array_list.Add(8);
+	array_list.Add(5);
+	array_list.Add(20);
+	array_list.Add(5);
+
+	int value = array_list.Get_Value_At_Index(-1);
+	REQUIRE(value == -1);
+}
+
+TEST_CASE("Test get value at index w 5")
+{
+const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+	array_list.Add(8);
+	array_list.Add(5);
+	array_list.Add(20);
+	array_list.Add(5);
+
+	int value = array_list.Get_Value_At_Index(5);
+	REQUIRE(value == -1);
+}
+
+TEST_CASE("Test deletion of value 5")
+{
+const int SIZE = 5;
+	int list[SIZE];
+
+	ArrayList array_list(list, SIZE);
+	REQUIRE(array_list.Size() == 0);
+	array_list.Add(5);
+	array_list.Add(7);
+	array_list.Add(10);
+	array_list.Add(15);
+	array_list.Add(20);
+	REQUIRE(array_list.Size() == SIZE);
+
+	array_list.Delete(5);
+	REQUIRE(array_list.Size() == (SIZE - 1));
+	REQUIRE(array_list.Get_Value_At_Index(0) == 7);
+	REQUIRE(array_list.Get_Value_At_Index(1) == 10);
+	REQUIRE(array_list.Get_Value_At_Index(2) == 15);
+	REQUIRE(array_list.Get_Value_At_Index(3) == 20);
+}
